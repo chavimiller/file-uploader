f9a35cfeb91de415098616d89ae0675939fb9b7e
chavimiller <chaviweisman@gmail.com>
2025-12-09 12:21:24 +0200

    Create and outline file controller and route

---
+++ controllers/fileController.js
@@ -0,0 +1,15 @@
+// const db = require("../?")
+
+async function newFileGet(req, res) {}
+
+async function newFilePost(req, res) {}
+
+async function readFile(req, res) {}
+
+async function updateFileGet(req, res) {}
+
+async function updateFilePost(req, res) {}
+
+async function deleteFile(req, res) {}
+
+module.exports = {};
---
+++ routes/fileRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const fileController = require("../controllers/fileController");
+const fileRouter = new Router();
+
+// GET new file
+
+// POST new file
+
+// GET read file
+
+// GET update file
+
+// POST update file
+
+// POST delete file
+
+module.exports = fileRouter;
