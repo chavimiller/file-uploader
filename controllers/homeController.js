2a3e813dd24e1afc73d438d332528bf2addb6d0f
chavimiller <chaviweisman@gmail.com>
2025-12-09 11:59:53 +0200

    Outline homepage controller and router

---
+++ controllers/homeController.js
@@ -0,0 +1,13 @@
+// const db = require("../?")
+
+// Display folders or files
+
+async function showAllContent(req, res) {}
+
+// Ensure user is logged in and if not redirect to login
+async function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  return res.redirect("/auth/login");
+}
+
+module.exports = { showAllContent, ensureAuthenticated };
---
+++ routes/homeRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const homeController = require("../controllers/homeController");
+const homeRouter = Router();
+
+// GET all content
+
+module.exports = homeRouter;
