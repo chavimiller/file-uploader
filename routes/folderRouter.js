13f85dd1e82e304c3ddc146d49530880d411563a
chavimiller <chaviweisman@gmail.com>
2025-12-10 14:47:09 +0200

    Add partial for adding new folder

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -3,9 +3,9 @@ const folderController = require("../controllers/folderController");
 const folderRouter = new Router();
 
 // GET new folder
-
+folderRouter.get("/new", folderController.newFolderGet);
 // POST new folder
-
+folderRouter.post("/new", folderController.newFolderPost);
 // GET edit folder
 
 // POST edit folder
