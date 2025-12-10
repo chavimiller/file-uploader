6ea7fdcc9ad1693a6110fb1715a1224ec3f8d270
chavimiller <chaviweisman@gmail.com>
2025-12-10 11:31:47 +0200

    Install bcrypt for password encryption

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,37 @@
-// const db = require("../?")
+const prisma = require("../lib/prisma");
+const { validationResult, matchedData } = require("express-validator");
 
 // Signup
 
 async function signUpGet(req, res) {
   try {
-    res.render("signup", { errors: [] });
+    res.render("signup", { errors: [], data: [] });
   } catch (err) {
     console.error("ERROR in signUpGet: ", err);
     res.status(500).send("Server error");
   }
 }
 
-async function signUpPost(req, res) {}
+async function signUpPost(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render("signup", {
+      errors: errors.array(),
+      data: req.body,
+    });
+  }
+  const userData = matchedData(req);
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: userData.name,
+        username: userData.username,
+        password: userData.password,
+      },
+    });
+  } catch (err) {}
+}
 
 // Login
 
